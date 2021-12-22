bceb4712b8fb2f8bd33b42c08c74316bf0dfd4c8
Marinos025 <takenogi@icloud.com>
2021-12-22 19:55:19 +0900

    DB構築中身はできていない

---
+++ app/Supporters.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Supporters extends Model
+{
+    //
+}
---
+++ database/migrations/2021_12_21_122117_create_message_chats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessageChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message_chats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->datetime('cretate_date');
+            $table->biginit('room_id');
+            $table->varchar('message',255);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message_chats');
+    }
+}
